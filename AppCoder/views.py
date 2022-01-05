0e9d884a77ed2f4bf0d5daedb3e65388ee64e969
balder86 <baldrtec@gmail.com>
2022-01-05 19:26:18 -0300

    first commit

---
+++ AppCoder/views.py
@@ -0,0 +1,12 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from AppCoder.models import Curso
+
+# Create your views here.
+def curso(self):
+
+    curso = Curso(nombre="Desarrollo Web", camada = 19881)
+    curso.save()
+    documentoDeTexto = f'--> Curso {curso.nombre} Camada: {curso.camada}'
+
+    return HttpResponse(documentoDeTexto)
